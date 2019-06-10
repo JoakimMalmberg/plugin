20bfae6b93a68792ccf0ac77d95e6eacf85a02ad
Joakim <jmalmberg.web@gmail.com>
2019-06-10 09:46:05 +0200

    year book start

---
+++ wcms18-year-book/includes/class-wcms18-year-book-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       m42.se
+ * @since      1.0.0
+ *
+ * @package    Wcms18_Year_Book
+ * @subpackage Wcms18_Year_Book/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wcms18_Year_Book
+ * @subpackage Wcms18_Year_Book/includes
+ * @author     Joakim Malmberg <jmalmberg.web@gmail.com>
+ */
+class Wcms18_Year_Book_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
